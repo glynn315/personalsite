ca9aebf3674b82e32b3f200d4f0ec9ae9eddba67
glynn315 <94496602+glynn315@users.noreply.github.com>
2025-06-09 17:28:02 +0800

    sample output

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      fontFamily:{
+        sans:['"Montserrat"', 'sans-serif']
+      }
+    },
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')
+,require('@tailwindcss/forms')
+,require('@tailwindcss/line-clamp')
+,require('@tailwindcss/typography')
+],
+};
